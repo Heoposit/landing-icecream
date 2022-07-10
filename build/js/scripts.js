d41e0485de5db09523b39b727302399a561b13f3
Heoposit <heopositfeed5209@gmail.com>
2022-07-10 13:45:38 +0500

    make header without adaptive

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -4,23 +4,27 @@
         const burger = document.querySelector('.burger')
         const menu = document.querySelector('.menu')
         const body = document.querySelector('body')
+        const button = document.querySelector('.button')
         burger.addEventListener('click', () => {
             if (!menu.classList.contains('active')) {
                 menu.classList.add('active')
                 burger.classList.add('active-burger')
                 body.classList.add('locked')
+                button.classList.add('active')
             } else {
                 menu.classList.remove('active')
                 burger.classList.remove('active-burger')
                 body.classList.remove('locked')
+                button.classList.remove('active')
             }
         })
         // Вот тут мы ставим брейкпоинт навбара
         window.addEventListener('resize', () => {
-            if(window.innerWidth > 991.98) {
+            if(window.innerWidth > 767.98) {
                 menu.classList.remove('active')
                 burger.classList.remove('active-burger')
                 body.classList.remove('locked')
+                button.classList.remove('active')
             }    
         })
     }
