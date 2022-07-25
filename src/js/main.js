77265640cf8c9174a74c495eeeb4d1f4bc7f80f6
Heoposit <heopositfeed5209@gmail.com>
2022-07-25 15:48:36 +0500

    make help page

--- src/js/main.js
+++ src/js/main.js
@@ -28,36 +28,3 @@
         })
     }
     burgerMenu();
-
-    const swiper = new Swiper('.swiper', {
-
-        // If we need pagination
-        pagination: {
-            el: '.swiper-pagination',
-        },
-    
-        // Navigation arrows
-        navigations: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-buttom-prev',
-        },
-    
-        // And if we need scrollbar
-        scrollbar: {
-            el: '.swiper-scrollbar',
-        },
-    
-        //  // Responsive breakpoints
-        //  breakpoints: {
-        //  // when window width is >= 320px
-        // 320 {
-        // slidesPerView: 3,
-        // spaceBetween: 20
-        // },
-        //  // when window width is >= 480px
-        // 480: {
-        // slidesPerView: 3,
-        // spaceBetween: 30
-        //  },
-        // }
-    })
